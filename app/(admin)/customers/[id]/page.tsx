8413bdfd56b4f1925d84ea695748d468126a7c2c
Suleman Awwal <awwal4md@gmail.com>
2026-07-10 09:13:22 +0100

    deposit

--- app/(admin)/customers/[id]/page.tsx
+++ app/(admin)/customers/[id]/page.tsx
@@ -12,15 +12,42 @@ export default async function CustomerPage({
   const { id } = await params;
   const customers = await allCustomers();
   const bookings = await getBooking();
-  const customerBookings = bookings.filter((booking) => booking.userId === id);
+  const customerBookings = bookings
+    .filter((booking) => booking.userId === id)
+    .map((booking) => ({
+      ...booking,
+      cost: booking.cost.toNumber(),
+      deposit: booking.deposit.toNumber(),
+      balance: booking.balance.toNumber(),
+      overpaid: booking.overpaid.toNumber(),
+      totalPrice: booking.totalPrice.toNumber(),
+      createdAt: booking.createdAt.toString(),
+      deliveryDate: booking.deliveryDate?.toString(),
+    }));
 
   const customer = customers.find((customer) => customer?.id === id);
 
   return (
     <div className="flex flex-col gap-4">
       <div>This is the customers {customer?.name}</div>
-      {customer ? <ProfileStats user={customer} /> : null}
-      <SearchCustomer customer={customer} customerBookings={customerBookings} />
+      {customer ? (
+        <>
+          <ProfileStats
+            user={{
+              ...customer,
+              cashBack: customer.cashBack.toString(),
+              totalCredit: customer.totalCredit.toString(),
+              totalDebits: customer.totalDebits.toString(),
+              totalSpent: customer.totalSpent.toString(),
+              phoneNumber: customer.phoneNumber ?? "",
+            }}
+          />
+          <SearchCustomer
+            customer={customer}
+            customerBookings={customerBookings}
+          />
+        </>
+      ) : null}
     </div>
   );
 }
