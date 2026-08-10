a80ee8941faf807f4717c473058673cfc1301aad
Suleman Awwal <awwal4md@gmail.com>
2026-08-10 14:21:52 +0100

    ui

--- app/(admin)/debt-cleared/page.tsx
+++ app/(admin)/debt-cleared/page.tsx
@@ -3,10 +3,15 @@ import { getDebtCleared } from "@/lib/actions/debt.actions";
 
 export default async function DebtCleared() {
   const debts = await getDebtCleared();
+  const allDebts = debts.map((debt) => ({
+    ...debt,
+    amount: Number(debt.amount),
+    createdAt: debt.createdAt.toString(),
+  }));
 
   return (
     <>
-      <Debt debts={debts} />
+      <Debt debts={allDebts} />
     </>
   );
 }
